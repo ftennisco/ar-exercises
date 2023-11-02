a71a0efd45f9e1903fcaa6077d9d6b9e7f4ba6f3
ftennisco <f.tennisco@hotmail.com>
2023-11-02 13:55:52 -0700

    implement exercise 2, update store name.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store (with id = 1) and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store (@store1) instance in the database
+@store1.update(name: 'New Store Name')
+
+# Verify the update by reloading @store1 from the database
+@store1.reload
+
+# Output the updated information
+puts "Updated store name: #{@store1.name}"
