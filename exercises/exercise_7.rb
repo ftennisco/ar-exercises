a6b87a892c92c282385da8bbdd281b9870fe7fd6
ftennisco <f.tennisco@hotmail.com>
2023-11-02 14:37:51 -0700

    implement exercise 7, Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+if new_store.save
+  puts "Store saved successfully!"
+else
+  puts "Failed to save the store. Errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
+
